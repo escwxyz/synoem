2ee5a363fe3ce9e3421c8f8193468ed31b600361
Jie <linsijia628@gmail.com>
2025-05-17 17:13:49 +0800

    chore: add missing layout

---
+++ apps/cms/src/app/layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from "next";
+import "./styles/custom.css";
+
+export const metadata: Metadata = {
+  title: "SynOEM Dashboard",
+  description: "SynOEM Dashboard",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className="flex min-h-screen flex-col antialiased">{children}</body>
+    </html>
+  );
+}
