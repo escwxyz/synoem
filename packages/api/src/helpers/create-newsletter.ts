8fe098ac5637f46dfb3942743aa29e8be1a1f2fe
Jie <linsijia628@gmail.com>
2025-05-18 16:12:27 +0800

    fix(api): newsletter creation

--- packages/api/src/helpers/create-newsletter.ts
+++ packages/api/src/helpers/create-newsletter.ts
@@ -32,7 +32,7 @@ export async function createNewsletterHelper(
 
     const result = await payload.create({
       collection: "newsletter-subscribers",
-      data: { email, ...rest },
+      data: { email, metadata: { ...rest } },
     });
 
     return {
